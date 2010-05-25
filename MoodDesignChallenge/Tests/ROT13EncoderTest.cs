7337eb95ebc682e263e438a3f7c43576293ef256
Justin Bozonier <darkxanthos@gmail.com>
2010-05-24 20:31:02 -0700

    Next phase of refactoring up next.

--- MoodDesignChallenge/Tests/ROT13EncoderTest.cs
+++ MoodDesignChallenge/Tests/ROT13EncoderTest.cs
@@ -32,61 +32,59 @@ namespace MoodDesignChallenge.Tests
         [Test]
         public void When_ROT13_encoding_an_uppercase_letter()
         {
-            string actualString = null;
             var expectedString = "N";
 
+            var textObserver = new TextObserver();
             var encoder = new ROT13Encoding();
-            encoder.Encode("A", encodedString => actualString = encodedString);
+            encoder.Encode("A", textObserver.Received);
 
-            Assert.That(actualString, Is.EqualTo(expectedString));
+            Assert.That(textObserver.ReceivedText, Is.EqualTo(expectedString));
         }
 
         [Test]
         public void When_ROT13_encoding_a_whitespace_character()
         {
-            string actualString = null;
             var expectedString = "\t";
 
+            var textObserver = new TextObserver();
             var encoder = new ROT13Encoding();
-            encoder.Encode("\t", encodedString => actualString = encodedString);
+            encoder.Encode("\t", textObserver.Received);
 
-            Assert.That(actualString, Is.EqualTo(expectedString));
+            Assert.That(textObserver.ReceivedText, Is.EqualTo(expectedString));
         }
 
         [Test]
         public void When_ROT13_encoding_a_multicharacter_string()
         {
-            string actualString = null;
             var expectedString = "no";
 
+            var textObserver = new TextObserver();
             var encoder = new ROT13Encoding();
-            encoder.Encode("ab", encodedString=>actualString = encodedString);
+            encoder.Encode("ab", textObserver.Received);
 
-            Assert.That(actualString, Is.EqualTo(expectedString));
+            Assert.That(textObserver.ReceivedText, Is.EqualTo(expectedString));
         }
 
         [Test]
         public void When_encoding_a_character_that_lies_between_Z_and_a_in_ascii()
         {
-            string actualString = null;
             var expectedString = ((char)('Z' + 1)).ToString();
-
+            var textObserver = new TextObserver();
             var encoder = new ROT13Encoding();
-            encoder.Encode(expectedString, encodedString=>actualString = encodedString);
+            encoder.Encode(expectedString, textObserver.Received);
 
-            Assert.That(actualString, Is.EqualTo(expectedString));
+            Assert.That(textObserver.ReceivedText, Is.EqualTo(expectedString));
         }
 
         [Test]
         public void When_encoding_a_character_that_lies_beyond_lowercase_z_in_ascii()
         {
-            string actualString = null;
             var expectedString = ((char) ('z' + 1)).ToString();
-
+            var textObserver = new TextObserver();
             var encoder = new ROT13Encoding();
-            encoder.Encode(expectedString, encodedString=>actualString = encodedString);
+            encoder.Encode(expectedString, textObserver.Received);
 
-            Assert.That(actualString, Is.EqualTo(expectedString));
+            Assert.That(textObserver.ReceivedText, Is.EqualTo(expectedString));
         }
     }
 }
