b7fc915acf3e2e1eedaa6496112bc84ac90b9545
Justin Bozonier <darkxanthos@gmail.com>
2010-05-24 23:51:06 -0700

    removed gui abstraction from the main project. The network object acts as our network configuration so it was redundant.

--- MoodDesignChallenge/RunNetwork.cs
+++ MoodDesignChallenge/RunNetwork.cs
@@ -5,26 +5,23 @@
         public static void RunSystem(IProcessedText guiWriter)
         {
             var configuration = new FileSystemConfiguration();
-            var guiActor = new SystemControlCenter();
             var encoder = new ROT13Encoding();
             var fileReader = new WholeFileAtOnceFileReader();
             var fileWriter = new WholeFileAtOnceFileWriter();
-            var encodedTextSubscribers = new IProcessedText[]
+            var encodedTextSubscribers = new[]
                                              {
-                                                 guiActor, 
+                                                 guiWriter, 
                                                  fileWriter
                                              }.CreateMultiObserver();
 
             configuration.SetFileReader(fileReader);
             configuration.SetFileWriter(fileWriter);
-            guiActor.WhenReadyToStartReadingNotify(fileReader);
-            guiActor.OnNewGUINotificationsNotify(guiWriter);
             fileReader.OnNewTextAvailableNotify(encoder);
             encoder.OnNewEncodedTextAvailableNotify(encodedTextSubscribers);
 
             configuration.Configure();
 
-            guiActor.Encode();
+            fileReader.Read();
         }
     }
 }
